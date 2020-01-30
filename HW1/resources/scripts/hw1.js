2faaf8efc9b22774a340904ef0743224abdc014c
Jerryj00210 <32402092+Jerryj00210@users.noreply.github.com>
2020-01-29 21:51:57 -0800

    Delete hw1.js

--- HW1/resources/scripts/hw1.js
+++
@@ -1,8 +0,0 @@
-var dialog = document.querySelector('dialog');
-function showDialog() {
-    console.log('Fired');
-    dialog.show();
-    setTimeout(function () {
-        dialog.close();
-      }, 3000);
-}
